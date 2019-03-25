7322d9abf668c48c1959c556406f7a81dd8a20ca
Xavier Lopez Reynau <xavi.lopez.reynau@gmail.com>
2019-03-25 22:17:04 +0100

    Improve visuals

--- JapaneseTree/Sakura.js
+++ JapaneseTree/Sakura.js
@@ -1,13 +1,16 @@
 
 var toRadian = Math.PI / 180;
 
+var leafs = 0;
+
 class Sakura {
     constructor (x, y, angle, length, depth, width) {
         this.root = new Branch(x, y, angle, length, depth, width);
+        this.root.isRoot = true
     }
 
     update (step) {
-        var increment = 3 * Math.cos(step) - 2;
+        var increment = 1.2 * Math.cos(step);
         this.root.update(increment);
     }
 
@@ -19,7 +22,7 @@ class Sakura {
 class Branch {
 
     constructor (x, y, angle, length, depth, max_width) {
-        this.seed = random(0.2, 0.6);
+        this.seed = random(0.2, 0.4);
         this.x = x;
         this.y = y;
         this.angle = angle;
@@ -30,17 +33,27 @@ class Branch {
 
         this.nx = this.x + this.length * Math.cos(this.angle * toRadian);
         this.ny = this.y - this.length * Math.sin(this.angle * toRadian);
-        
 
         this.childs = [];
-        if (this.depth > 0) {
+        if (this.depth > 1) {
             var max_len = this.length / 3 * 2;
             var min_len = max_len/5 * 4
+
             this.childs.push(new Branch(this.nx - this.width/3, this.ny, this.angle + 30, random(min_len, max_len), this.depth-1, this.width));
-            this.childs.push(new Branch(this.nx * random(0.2, 0.8)  - this.width/3, this.ny, this.angle + random_round(10, 20), random(min_len/2, max_len), this.depth-1, this.width));
+            this.childs.push(new Branch(this.nx - this.width/3, this.ny * random(0.2, 0.8), this.angle + random_round(10, 20), random(min_len/3, max_len), this.depth-1, this.width));
 
             this.childs.push(new Branch(this.nx + this.width/3, this.ny, this.angle - 30, random(min_len, max_len), this.depth-1, this.width));
-            this.childs.push(new Branch(this.nx * random(0.2, 0.8)  + this.width/3, this.ny, this.angle - random_round(10, 20), random(min_len/3, max_len), this.depth-1, this.width));
+            this.childs.push(new Branch(this.nx + this.width/3, this.ny * random(0.2, 0.8), this.angle - random_round(10, 20), random(min_len/3, max_len), this.depth-1, this.width));
+        }
+        else if (this.depth > 0) {
+            var max_len = this.length / 3 * 2;
+            var min_len = max_len/5 * 4
+
+            this.childs.push(new Branch(this.nx - this.width/3, this.ny, this.angle + 60, random(min_len, max_len), this.depth-1, this.width));
+            this.childs.push(new Branch(this.nx - this.width/3, this.ny * random(0.2, 0.8), this.angle + random_round(50, 80), random(min_len/3, max_len), this.depth-1, this.width));
+
+            this.childs.push(new Branch(this.nx + this.width/3, this.ny, this.angle - 60, random(min_len, max_len), this.depth-1, this.width));
+            this.childs.push(new Branch(this.nx + this.width/3, this.ny * random(0.2, 0.8), this.angle - random_round(50, 80), random(min_len/3, max_len), this.depth-1, this.width));
         }
         else {
             this.childs.push(new Leaf(this.nx, this.ny));
@@ -53,9 +66,12 @@ class Branch {
     }
 
     update (step) {
-        this.mod_angle = this.angle + step;
-        this.nx = this.x + this.length * Math.cos(this.mod_angle * toRadian);
-        this.ny = this.y - this.length * Math.sin(this.mod_angle * toRadian);
+        if (!this.isRoot) {
+            this.mod_angle = this.angle + step;
+            this.nx = this.x + this.length * Math.cos(this.mod_angle * toRadian);
+            this.ny = this.y - this.length * Math.sin(this.mod_angle * toRadian);
+        }
+        
 
         if (this.childs.length >= 2) {
             for (let i = 0; i < this.childs.length; ++i) {
@@ -79,14 +95,14 @@ class Branch {
     draw () {
         ctx.beginPath();
         ctx.lineWidth = this.width > 0 ? this.width : 1;
-        ctx.strokeStyle = "#7f6e9b";
+        ctx.strokeStyle = "#8D89A6";
         ctx.moveTo(this.x, this.y);
         ctx.lineTo(this.nx, this.ny);
         ctx.stroke();
 
         ctx.beginPath();
         ctx.lineWidth = this.width > 0 ? this.width/3*2 : 1;
-        ctx.strokeStyle = "#b7abcc";
+        ctx.strokeStyle = "#BFABCB";
         ctx.moveTo(this.x + this.width/2, this.y);
         ctx.lineTo(this.nx + this.width/2, this.ny);
         ctx.stroke();
@@ -106,6 +122,8 @@ class Leaf {
         this.sr = this.r/3*2;
         this.ox = this.sr/random(2,3) * (random_round(0,1) ? -1 : 1);
         this.oy = -this.sr/random(2,3);
+
+        leafs += 1;
     }
 
     update (x, y) {
@@ -114,11 +132,11 @@ class Leaf {
     }
 
     draw () {
-        ctx.fillStyle = "#ffdbfd";
+        ctx.fillStyle = "#ffc1e5";
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
         ctx.fill();
-        ctx.fillStyle = "#ffedfc";
+        ctx.fillStyle = "#ffe8f4";
         ctx.beginPath();
         ctx.arc(this.x + this.ox, this.y + this.oy, this.sr, 0, 2 * Math.PI);
         ctx.fill();
